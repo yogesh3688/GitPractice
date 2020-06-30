8f6aff4ff26c110fcac637cd52b1edb6571dcfaa
Yogesh <yogeshgnipate@yahoo.com>
2020-06-30 14:41:13 +0530

    F added addStepTwo

--- src/com/base/first/First.java
+++ src/com/base/first/First.java
@@ -6,5 +6,13 @@ public class First {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 1");
 	}
+	
+	public void addStepOne() {
+		System.out.println("Step added");
+	}
+	
+	public void addStepTwo() {
+		System.out.println("test");
+	}
 
 }
